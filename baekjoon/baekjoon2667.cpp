5740dcceabbfe2b2690c5fdf677fb5c23e4abf96
Pilse <gogosky1175@gmail.com>
2021-01-14 00:25:18 +0900

    0113_web_&_baekjoon2667

---
+++ baekjoon/baekjoon2667.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+char map[26][26];
+int visited[26][26];
+int N;
+int total;
+int countNumber;
+vector<pair<int,int>> vec[26][26];
+queue<pair<int,int>> q;
+
+int bfs(pair<int,int> house)
+{
+    visited[house.first][house.second]=1;
+    q.push({house.first,house.second});
+    while(!q.empty())
+    {
+        countNumber++;
+        pair<int,int> p=q.front();
+        q.pop();
+        for(int i=0;i<vec[p.first][p.second].size();i++)
+        {
+            pair<int,int> w = vec[p.first][p.second][i];
+            if(!visited[w.first][w.second])
+            {
+                visited[w.first][w.second]=1;
+                q.push(w);
+            }
+        }
+    }
+    return countNumber;
+}
+
+int main()
+{
+    cin >> N;
+    int index=0;
+    int cnt[676] ={0,};
+    
+    for(int j=1;j<N+1;j++)
+    {
+        scanf("%s",map[j]+1);
+    }
+
+    for(int i=1;i<N+1;i++)
+    {
+        for(int j=1;j<N+1;j++)
+        {
+            if(map[i][j]=='1')
+            {
+                if(map[i-1][j]=='1')
+                {
+                    vec[i][j].push_back({i-1,j});
+                    vec[i-1][j].push_back({i,j});
+                }
+                if(map[i+1][j]=='1')
+                {
+                    vec[i][j].push_back({i+1,j});
+                    vec[i+1][j].push_back({i,j});
+                }
+                if(map[i][j-1]=='1')
+                {
+                    vec[i][j].push_back({i,j-1});
+                    vec[i][j-1].push_back({i,j});
+                }
+                if(map[i][j+1]=='1')
+                {
+                    vec[i][j+1].push_back({i,j});
+                    vec[i][j].push_back({i,j+1});
+                }
+             
+            }
+        }
+    }
+    for(int i=1;i<N+1;i++)
+    {
+        for(int j=1;j<N+1;j++)
+        {
+            if(!visited[i][j] && map[i][j]=='1')
+            {
+                cnt[index++]=bfs({i,j});
+                total++;
+                countNumber=0;
+            }
+        }
+    }
+    sort(cnt,cnt+index);
+    cout<<total<<endl;
+    for(int i=0;i<total;i++)
+    {
+        cout<<cnt[i]<<endl;
+    }
+
+}
--- web/javascript-graffiti/script.js
+++ web/javascript-graffiti/script.js
@@ -53,10 +53,108 @@
 // };
 // lifeInWeeks(30);
 
-function bmiCalculator (height,weight){
-    var bmi = height/(weight*weight); // is same with height/Math.pow(height,2);
-    return bmi;
-}
+// function bmiCalculator (height,weight){
+//     var bmi = height/(weight*weight); // is same with height/Math.pow(height,2);
+//     return bmi;
+// }
 
-var bmi = bmiCalculator(65,1.8);
-console.log(Math.round(bmi));
+// var bmi = bmiCalculator(65,1.8);
+// console.log(Math.round(bmi));
+
+// var n =Math.random()*10;
+// console.log(Math.round(n));
+
+// var name1 = prompt("love percentage! first:");
+// var name2 = prompt("second:");
+// var randomNumber = Math.random()*100+1;
+// alert(randomNumber.toFixed(1)+"% !");
+
+// function bmi(weight,height){
+//     var BMI = weight/Math.pow(height,2);
+//     var interpretation="";
+//     if(BMI<18.5)
+//         interpretation += "Your BMI is "+BMI+",so you are underweight";
+//     else if(BMI>18.5 && BMI <24.9)
+//         interpretation += "Your BMI is "+BMI+",so you have noraml weight";
+//     else
+//         interpretation+="Your BMI is "+BMI+",so you are overweight";
+
+//     return interpretation;
+// }
+// console.log(bmi(72,1.81));
+
+// function isLeap(year){
+//     var isLeap="";
+//     if(year%4===0){
+//         if(year%100===0){
+//             if(year%400===0)
+//                 isLeap+="Leap year."
+//             else
+//                 isLeap+="Not leap year."
+//         }
+//         else
+//             isLeap+="Leap year."
+//     }
+//     else
+//         isLeap+="Not leap year."
+//     return isLeap;
+// }
+// console.log(isLeap(2020));
+
+// var guest =["a","b","c","d","e"];
+// var isGuest=prompt("who are you");
+// if(guest.includes(isGuest))
+//     console.log("welcome");
+// console.log(guest.length);
+// guest.push("f");
+// console.log(guest);
+// guest.pop();
+// console.log(guest);
+
+// var output=[];
+// var index=1;
+// function fizzBuzz(){
+//     var string="";
+//     if(index%3===0)
+//         string="Fizz";
+//     if(index%5===0)
+//         string="Buzz";
+//     if(index%3===0&&index%5===0)
+//         string="FizzBuzz";
+
+//     if(string.length>0)
+//         output.push(string);
+//     else
+//         output.push(index);
+//     index++;
+//     console.log(output);
+// }
+
+// for(var i=0;i<15;i++){
+//     fizzBuzz();
+// }
+
+// function whosPaying(names){
+//     var randomNumber = Math.floor(Math.random()*(names.length));
+//     console.log(randomNumber);
+//     return names[randomNumber]+" is going to buy lunch today!";
+// }
+// console.log(whosPaying(["a","b","c","d","e"]));
+
+function fibonacciGenerator(n){
+    var first=0;
+    var second=1;
+    var output=[];
+    for(var i=0;i<n;i++)
+    {
+        if(i===0)
+            output.push(first);
+        else if(i===1)
+            output.push(second);
+        else
+            output.push((output[output.length-1])+(output[output.length-2]))
+    }
+    return output;
+}
+output=fibonacciGenerator(5);
+console.log(output);
