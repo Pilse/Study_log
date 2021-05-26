69dba72bc59e11e135f4f02cce20c02c1ec4eb35
pilse <gogosky1175@gmail.com>
2021-05-26 16:37:34 +0900

    vue

---
+++ vue/vue-lifecycle/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+// How Vue updates data
+
+// const data ={
+//   message: 'Hello!',
+//   longMessage: 'Hello! World!'
+// };
+// const handler = {
+//   set(target, key, value){
+//     if(key === 'message') {
+//       target.longMessage = value + 'World!';
+//     }
+//   }
+// };
+// const proxy = new Proxy(data, handler);
