e5c8e1795a62adba8df9673271f1a005b0065783
pilse <gogosky1175@gmail.com>
2021-07-02 23:37:14 +0900

    react

--- react/proshop/front/src/components/App.jsx
+++ react/proshop/front/src/components/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 import HomeScreen from '../screens/HomeScreen'
+import ProductScreen from '../screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
         <main className="py-3">
           <Container>
-            <HomeScreen />
+            <Route path='/' exact component={HomeScreen} />
+            <Route path='/product/:id' exact component={ProductScreen} />
           </Container>
         </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- react/proshop/front/src/components/Product.jsx
+++ react/proshop/front/src/components/Product.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}></a>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top"></Card.Img>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+      </Card.Body>
+
+      <Card.Text as='div'>
+        <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+      </Card.Text>
+
+      <Card.Text as='h3'> ${product.price} </Card.Text>
     </Card>
   )
 }
