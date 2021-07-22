5b92b2b23975a20a3a1ead84037b021a5db45627
pilse <gogosky1175@gmail.com>
2021-07-22 23:52:12 +0900

    react & baekjoon

--- react/proshop/back/controllers/productController.js
+++ react/proshop/back/controllers/productController.js
@@ -89,6 +89,7 @@ const createProductReview = asyncHandler (async (req, res) => {
 
   const product = await Product.findById(req.params.id)
   if(product) {
+    console.log(product.reviews)
     const alereadyReviewd = product.reviews.find(r => r.user.toString() === req.user._id.toString())
 
     if(alereadyReviewd){
@@ -97,7 +98,7 @@ const createProductReview = asyncHandler (async (req, res) => {
     }
 
     const review = {
-      name: req.user.nmae,
+      name: req.user.name,
       rating: Number(rating),
       comment,
       user: req.user._id
@@ -107,10 +108,10 @@ const createProductReview = asyncHandler (async (req, res) => {
 
     product.numReviews = product.reviews.length
     product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0)
-    / products.reviews.length
+    / product.reviews.length
 
     await product.save()
-    res.status(201).json({ message: 'Review added' })
+    res.status(201).json({ success: 'Review added' })
   } else {
     res.status(404)
     throw new Error('Product not found')
