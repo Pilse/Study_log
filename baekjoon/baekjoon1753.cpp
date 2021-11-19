5781ee556bb867f3120e488f4581adfff6ffe9d7
pilse <gogosky1175@gmail.com>
2021-11-19 23:58:32 +0900

    baekjoon

--- baekjoon/baekjoon1753.cpp
+++ baekjoon/baekjoon1753.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <climits>
-#include <string.h>
+#define INF 987654321
 
 using namespace std;
 
-vector<pair<int, int> > vec[20001];
-priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > q;
-int dist[20001];
+vector<pair<int, int>> vec[20001];
 int v, e;
+int dist[20001];
 
 void dijk(int s)
 {
+    for(int i = 1; i <= v; i++)
+        dist[i] = INF;
     dist[s] = 0;
-    q.push({0, s});
-
-    while (!q.empty())
+    
+    priority_queue<pair<int, int>> pq;
+    
+    pq.push({0, s});
+    
+    while(!pq.empty())
     {
-        int distance = q.top().first;
-        int p = q.top().second;
-        q.pop();
-
-        for (int i = 0; i < vec[p].size(); i++)
+        int w = -pq.top().first;
+        int c = pq.top().second;
+        pq.pop();
+        
+        if(w > dist[c]) continue;
+        
+        for(int i = 0; i < vec[c].size(); i++)
         {
-            int w = vec[p][i].first;
-            int d = vec[p][i].second;
-            if (dist[w] <= distance + d)
-                continue;
-            dist[w] = distance + d;
-            q.push({dist[w], w});
+            int nc = vec[c][i].first;
+            int nw = vec[c][i].second;
+            
+            if(w + nw < dist[nc])
+            {
+                dist[nc] = w + nw;
+                pq.push({-(nw + w), nc});
+            }
         }
     }
 }
@@ -37,30 +44,28 @@ void dijk(int s)
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int start;
-
+    cin.tie(0);
+    
+    int s;
+    
     cin >> v >> e;
-    cin >> start;
-
-    int t1, t2, t3;
-    for (int i = 0; i < e; i++)
+    cin >> s;
+    
+    int t1, t2, tw;
+    
+    for(int i = 0; i < e; i++)
     {
-        cin >> t1 >> t2 >> t3;
-        vec[t1].push_back({t2, t3});
+        cin >> t1 >> t2 >> tw;
+        vec[t1].push_back({t2, tw});
     }
-
-    memset(dist, 555819297, sizeof(int) * (v + 1));
-    dijk(start);
-
-    for (int i = 1; i <= v; i++)
+    
+    dijk(s);
+    
+    for(int i = 1; i <= v; i++)
     {
-        if (dist[i] == 555819297)
-        {
+        if(dist[i] == INF)
             cout << "INF" << '\n';
-            continue;
-        }
-        cout << dist[i] << '\n';
+        else
+            cout << dist[i] << '\n';
     }
 }
