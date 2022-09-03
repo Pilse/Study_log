241e6a53027f99f49bd929dbb272712385c331cd
pilse <gogosky1175@gmail.com>
2022-09-03 13:44:16 +0900

    baekjoon

--- baekjoon/baekjoon9251.cpp
+++ baekjoon/baekjoon9251.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
 using namespace std;
-
-
-
-int dp[1010];
-int pre[1010];
 
 int main()
 {
-    string str1;
-    string str2;
-    
-    int cnt;
-    
-    cin>>str1;
-    cin>>str2;
-    
-    for(int i=0;i<str2.length();i++)
-    {
-        for(int j=0;j<str1.length();j++)
-        {
-            if(str1[j]==str2[i])
-            {
-                cnt=pre[j-1];
-                
-                for(int k=j;k<str1.length();k++)
-                {
-                    if(dp[k]>=cnt+1)
-                        continue;
-                    dp[k]=cnt+1;
-                }
-            }
-        }
-        for(int j=0;j<str1.length();j++)
-        {
-            pre[j]=dp[j];
-        }
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    vector<vector<int>> dp(str1.length() + 1, vector<int>(str2.length() + 1, 0));
+
+    for (int i = 1; i <= str1.length(); i++)
+    {
+        for (int j = 1; j <= str2.length(); j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                continue;
+            }
+
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
     }
-    cout<<dp[str1.length()-1];
-    
+
+    cout << dp[str1.length()][str2.length()];
 }
